e2e85d578bc588d0b1e59d98f9860793c2d07f3c
Mathieu Auclair <mathiass12@hotmail.com>
2016-11-02 17:30:34 -0700

    Make it rain!

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-function drawRain(x,y,color)
+function drawRain(x,y,width,height,color)
 {
 	context.beginPath();
-	context.rect(x, y, 3, 10);
+	context.rect(x, y, width, height);
 	context.fillStyle = color;
 	context.fill();
 }
 	
 var cloudFrameRate = 10;
-var cloud = "50"
+var cloud = 50;
 	
 function rainDrop()
 {
 	this.yPosition = 0-Math.random()*500;
 	this.xPosition = canvas.width;
 	this.ySpeed = 5;
+	this.rainHeight = 10;
 }
 
 var rainDropList = [];
 
-for(i=0;i<cloud;i++)//give a value to the array
-	{
-		rainDropList[i]=new rainDrop();
-		rainDropList[i].xPosition = Math.random()*canvas.width;
-		rainDropList[i].ySpeed = ((Math.random()*3)+4);
-		
-	}
 
+for(i=0;i<cloud;i++)//give a value to the array
+{
+	rainDropList[i]=new rainDrop();
+	rainDropList[i].xPosition = Math.random()*canvas.width;
+	rainDropList[i].ySpeed = ((Math.random()*3)+4);
+	rainDropList[i].rainHeight = ((Math.random()*10)+10);
+}
 
 var gameLoop = setInterval(function(){
 	context.clearRect(0,0,canvas.width,canvas.height);
@@ -38,6 +39,6 @@ var gameLoop = setInterval(function(){
 		rainDropList[i].yPosition += rainDropList[i].ySpeed;
 		else
 		rainDropList[i].yPosition = 0-Math.random()*50;
-		drawRain(rainDropList[i].xPosition,rainDropList[i].yPosition,"blue");
+		drawRain((rainDropList[i].xPosition),(rainDropList[i].yPosition),(rainDropList[i].ySpeed/1.75),(rainDropList[i].rainHeight),"blue");
 	}
 },10);//frameRate
