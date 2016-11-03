63910821138670f3b7465ac03b7f490eed44fc4e
Mathieu Auclair <mathiass12@hotmail.com>
2016-11-02 17:15:19 -0700

    Spread rain drop array + add random speed

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
-function drawRain(x,y)
+function drawRain(x,y,color)
 {
-	context.clearRect(0,0,canvas.width,canvas.height);
 	context.beginPath();
 	context.rect(x, y, 3, 10);
-	context.fillStyle = "blue";
+	context.fillStyle = color;
 	context.fill();
 }
 	
@@ -15,27 +14,30 @@ var cloud = "50"
 	
 function rainDrop()
 {
-	this.yPosition = -50;
-	this.xPosition = canvas.width;//Math.random()*canvas.width
-	this.ySpeed = 10/cloudFrameRate;
+	this.yPosition = 0-Math.random()*500;
+	this.xPosition = canvas.width;
+	this.ySpeed = 5;
 }
 
 var rainDropList = [];
 
-//give a value to the array
-for(i=0;i<cloud;i++)
+for(i=0;i<cloud;i++)//give a value to the array
 	{
 		rainDropList[i]=new rainDrop();
+		rainDropList[i].xPosition = Math.random()*canvas.width;
+		rainDropList[i].ySpeed = ((Math.random()*3)+4);
+		
 	}
 
 
 var gameLoop = setInterval(function(){
+	context.clearRect(0,0,canvas.width,canvas.height);
 	for(i=0;i<rainDropList.length;i++)
 	{
-		drawRain(rainDropList[i].xPosition,rainDropList[i].yPosition);
 		if(rainDropList[i].yPosition<canvas.height)
 		rainDropList[i].yPosition += rainDropList[i].ySpeed;
 		else
-		rainDropList[i].yPosition = -50;
+		rainDropList[i].yPosition = 0-Math.random()*50;
+		drawRain(rainDropList[i].xPosition,rainDropList[i].yPosition,"blue");
 	}
-},cloudFrameRate);
+},10);//frameRate
